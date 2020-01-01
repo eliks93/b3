06014edb910187f493744e9dd6796cc708ec162e
Eli Kittross-Schnell <eliks92@gmail.com>
2020-01-01 12:41:13 -0800

    added NPC healthbar and some basic shaders to make it look different

--- client/Bar.gd
+++ client/Bar.gd
@@ -1,13 +1,7 @@
 extends HBoxContainer
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	set_as_toplevel(true)
-	add_shader_override(
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -16,4 +10,3 @@ func _process(delta):
 	global_position[1] -= 230
 	global_position[0] -= 200
 	rect_global_position = global_position
-
--- client/NPCBoat.gd
+++ client/NPCBoat.gd
@@ -2,19 +2,26 @@ extends "res://Boat.gd"
 
 signal turn_turret(mouse_position)
 
+onready var bar = $Bar/TextureProgress
+onready var tween = $Tween
+
+var animated_health = 0
 var mouse_position
 
 var player_init = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var npc_max_health = hp
+	bar.max_value = npc_max_health
+	update_health(npc_max_health)
 
 slave func setPosition(packet):
 	# Apply movement to other boats.
 	pass
 
-#func _process(delta):
+func _process(delta):
+	bar.value = animated_health
 #	emit_signal("turn_turret", mouse_position)
 
 #var packet = {
@@ -27,3 +34,11 @@ slave func setPosition(packet):
 #	'acceleration': self.acceleration,
 #	'velocity': self.velocity
 #}
+
+func _on_PlayerBoat_health_changed(npc_health):
+	update_health(npc_health)
+
+func update_health(new_value):
+	tween.interpolate_property(self, "animated_health", animated_health, new_value, 0.6, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	if not tween.is_active():
+		tween.start()
