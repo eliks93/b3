fa7ff04f3ab0da66d30e124f901e672b4a5f5330
Eli Kittross-Schnell <eliks92@gmail.com>
2019-12-31 17:24:37 -0800

    added animation to hp-bar

--- client/RealPlayer.gd
+++ client/RealPlayer.gd
@@ -3,6 +3,9 @@ extends "res://Boat.gd"
 signal fire_turret(group)
 signal turn_turret(mouse_pos)
 onready var bar = $Bar/TextureProgress
+onready var tween = $Tween
+
+var animated_health = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Turret1.connect("spawn_projectile", self, "_spawn_projectile")
@@ -14,11 +17,10 @@ func _ready():
 
 func _process(delta):
 	emit_signal("turn_turret", get_global_mouse_position())
+	bar.value = animated_health
 
 func get_input():
 	var turn = 0
-	if Input.is_action_just_pressed("space"):
-		take_damage(10)
 	if Input.is_action_pressed("steer_right"):
 		turn += 1
 	if Input.is_action_pressed("steer_left"):
@@ -39,7 +41,9 @@ func _on_PlayerBoat_health_changed(player_health):
 	update_health(player_health)
 
 func update_health(new_value):
-	print("update?")
-	bar.value = new_value
+	tween.interpolate_property(self, "animated_health", animated_health, new_value, 0.6, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	if not tween.is_active():
+		tween.start()
+
 
 	
