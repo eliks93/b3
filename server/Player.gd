792f6692df923292b31dd47538e4ea2090588e25
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2020-01-08 18:20:50 -0800

    increased the map size added more spawnpoints that use the number of players to determine the spawn

--- server/Player.gd
+++ server/Player.gd
@@ -75,7 +75,6 @@ remote func update_health(hp, p_owner):
 			print(current_health, "current health")
 			get_node("..").set_score(p_owner)
 		$PlayerBoat.queue_free()
-			
 		temp_score += get_parent().leaderboard[player_id]['score']
 		if current_health > 0:
 			print("score was prior to update", temp_score)
@@ -97,10 +96,24 @@ remote func respawn():
 	var x = 0
 	var y = 0
 	var available_spawns = []
-	for point in get_parent().get_node("Map01").get_node("Spawns").get_children():
-		if point.available:
-			available_spawns.append(point.position)
+	var num_players = get_parent().players.size()
+	var quater_limit
+	var index = 0
+	if num_players < 8:
+		quater_limit = 1
+	elif num_players < 16:
+		quater_limit = 2
+	elif num_players < 16:
+		quater_limit = 3
+	else:
+		quater_limit = 4
+	while index < quater_limit:
+		for point in get_parent().get_node("Map01").get_node("Spawns").get_child(index).get_children():
+			if point.available:
+				available_spawns.append(point.position)
+		index += 1
 	var spawn = available_spawns[int(rand_range(0,(available_spawns.size())-1))]
+	print(available_spawns)
 	var rotation = 0
 	var new_boat = boat.instance()
 	new_boat.position.x = x
