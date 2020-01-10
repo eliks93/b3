3c74a973003af70628381ae35f6faf36e074079c
Eli Kittross-Schnell <eliks92@gmail.com>
2020-01-10 15:04:20 -0800

    refactored splash screen slightly

--- client/Splash.gd
+++ client/Splash.gd
@@ -1,11 +1,9 @@
 extends Node
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var touch = OS.has_touchscreen_ui_hint()
 
 var MainMenu = preload("res://MainMenu.tscn")
-
+	
+	
 func _on_AudioController_start():
 	get_node("..").swap_scene(get_node("."), MainMenu.instance())
 
--- client/labelTitle3.gd
+++ client/labelTitle3.gd
@@ -6,8 +6,7 @@ extends Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var ui = get_node('../UI')
-	if ui.has_node('TouchControls'):
+	if get_parent().touch:
 		self.text = ""
 	else:
 		self.text = 'Use right and left mouse button to fire primary and secondary projectiles!'
