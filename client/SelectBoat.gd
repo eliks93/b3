6db04e09733cf637cc941ca0387d97d89b7634aa
Eli Kittross-Schnell <eliks92@gmail.com>
2020-01-09 19:21:31 -0800

    fixed some duuumb bugs

---
+++ client/SelectBoat.gd
@@ -0,0 +1,14 @@
+extends KinematicBody2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Bar.hide()
+	$PlayerName.hide()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
