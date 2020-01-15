fd6d6030e335148739833058f79a446dcf0c709a
Eli Kittross-Schnell <eliks92@gmail.com>
2020-01-15 15:15:31 -0800

    fixed fatal error

--- client/ScoreList.gd
+++ client/ScoreList.gd
@@ -5,9 +5,6 @@ extends ItemList
 # var b = "text"
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	set_icon_mode(ICON_MODE_RIGHT)
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
