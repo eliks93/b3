15c6e2cee2482d26a94b26bd47c36969a33a1778
Robert Beckett <robert.beckett@onion.red>
2020-01-16 14:59:06 -0800

    Mobile revert.

--- client/SpecialAbility.gd
+++ client/SpecialAbility.gd
@@ -9,22 +9,11 @@ func _ready():
 	fake_event.action = "fire_2"
 
 
-#func _on_SpecialAbility_gui_input(event):
-#	if event is InputEventScreenTouch and event.is_pressed():
-#		fake_event.pressed = true
-#		Input.parse_input_event(fake_event)
-#		ongoing_touch = event.get_index()
-#	if event is InputEventScreenTouch and !event.is_pressed() and event.get_index() == ongoing_touch:
-#		fake_event.pressed = false
-#		Input.parse_input_event(fake_event)
-
-func _on_SpecialAbility_pressed():
-	fake_event.pressed = true
-	Input.parse_input_event(fake_event)
-	$Timer.wait_time = 0.25
-	$Timer.start()
-
-
-func _on_Timer_timeout():
-	fake_event.pressed = false
-	Input.parse_input_event(fake_event)
+func _on_SpecialAbility_gui_input(event):
+	if event is InputEventScreenTouch and event.is_pressed():
+		fake_event.pressed = true
+		Input.parse_input_event(fake_event)
+		ongoing_touch = event.get_index()
+	if event is InputEventScreenTouch and !event.is_pressed() and event.get_index() == ongoing_touch:
+		fake_event.pressed = false
+		Input.parse_input_event(fake_event)
